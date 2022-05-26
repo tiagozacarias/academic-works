bac789f4c6c1295117b7a9dba4321dd29625a336
Tiago Eduardo Zacarias <tiago@zacarias.tiagoti.com.br>
2022-05-25 22:17:22 -0300

    Commit Update

--- work_otavio/estoque_01.py
+++ work_otavio/estoque_01.py
@@ -17,163 +17,148 @@ def id_pessoal():
 
     print(f"Bem Vindo ao Controle de estoque da Bicicletaria do Otavio Augusto")
 
-# Class Principal 
-class principal:
+# Class Principal
+class Principal:
 
+    def __init__(self) -> None:
+        pass
 
     def buscar_codigo(val):
-                    
+
         for chave, value in cadastro_codigo.items():
 
             if val == value:
 
                 print(f"-------------------------\n")
                 print(f"Código : {cadastro_codigo[chave]:03d}")
-                print(f"Nome  : {chave}")                   
-                print(f"Fabricante : {cadastro_nome_fab_peca[chave]}")                    
+                print(f"Nome  : {chave}")
+                print(f"Fabricante : {cadastro_nome_fab_peca[chave]}")
                 print("Valor : %.1f" % (cadastro_valor[chave]))
                 print(f"-------------------------\n")
-    
-    
+
     def buscar_fabricante(val):
-                    
+
         for chave, value in cadastro_nome_fab_peca.items():
 
             if val == value:
 
                 print(f"-------------------------\n")
                 print(f"Código : {cadastro_codigo[chave]:03d}")
-                print(f"Nome   : {chave}")                   
-                print(f"Fabricante : {cadastro_nome_fab_peca[chave]}")                    
+                print(f"Nome   : {chave}")
+                print(f"Fabricante : {cadastro_nome_fab_peca[chave]}")
                 print("Valor : %.1f" % (cadastro_valor[chave]))
                 print(f"-------------------------\n")
 
-
     def remover_item(val):
-                    
+
         for chave, value in cadastro_codigo.items():
 
             if val == value:
-            
-                cadastro_codigo.pop(chave)                  
-                cadastro_nome_fab_peca.pop(chave)                    
+
+                cadastro_codigo.pop(chave)
+                cadastro_nome_fab_peca.pop(chave)
                 cadastro_valor.pop(chave)
-          
-            principal.consultarPeca()
-            
-    
+
+            Principal.consultarPeca()
 
     def menu():
-    
 
         while True:
 
             try:
 
-
-                print("""
-Escolha a opção desejada:
-
-1  - Cadastrar Peças
-2  - Consultar Peças
-3  - Remover Peças
-4  - Sair""")
+                print("Escolha a opção desejada:")
+                print("1  - Cadastrar Peças")
+                print("2  - Consultar Peças")
+                print("3  - Remover Peças")
+                print("4  - Sair")
 
                 opcao = int(input())
-    
+
                 if opcao == 1:
-                
+
                     print("Você Selecionou a opção de cadastrar peça")
 
                     # Procura ultimo indice da lista
-                    cont_indice =  len(contador) -1
+                    cont_indice = len(contador) - 1
                     cont_f = contador[cont_indice]
 
                     # Cria novo codigo (Gerador de Codigo)
-                    cont = cont_f + 1 
+                    cont = cont_f + 1
                     contador.append(cont)
-                    principal.cadastrarPeca(cont)
-            
+                    Principal.cadastrarPeca(cont)
 
                 if opcao == 2:
 
-                    principal.consultarPeca()
-            
+                    Principal.consultarPeca()
+
                 if opcao == 3:
 
-                    principal.removerPeca()
+                    Principal.removerPeca()
 
                 if opcao == 4:
-                
-                    break 
-            
-                                    
+
+                    break
+
             except(UnboundLocalError, ValueError):
 
                 print(
                     """Você digitou uma opção invalida\nPor favor entre com a opção desejada novamente\n""")
 
-
     def cadastrarPeca(cont):
 
-                print(f"Código da Peça: {cont:03d}")
-        
-                nome = str(input("Por Favor entre com o NOME da peça:"))
-                fabricante = str(input("Por Favor entre com o FABRICANTE da peça:"))
-                valor = int(input("Por Favor entre com o VALOR(R$) da peça:"))
-        
-                # Cadastro de codigo da peca, fabricante e valor
-                cadastro_codigo[nome] = cont
-                cadastro_nome_fab_peca[nome] = fabricante 
-                cadastro_valor[nome] = valor
-        
+        print(f"Código da Peça: {cont:03d}")
+
+        nome = str(input("Por Favor entre com o NOME da peça:"))
+        fabricante = str(input("Por Favor entre com o FABRICANTE da peça:"))
+        valor = int(input("Por Favor entre com o VALOR(R$) da peça:"))
+
+        # Cadastro de codigo da peca, fabricante e valor
+        cadastro_codigo[nome] = cont
+        cadastro_nome_fab_peca[nome] = fabricante
+        cadastro_valor[nome] = valor
 
     def consultarPeca():
-        
+
         while True:
 
             try:
 
-                print("""
-Você Selecionou a Opção de Consultar Peças
-Escolha a opção desejada:
-1   - Consultar Todas as Peças
-2   - Consulta Peças por Código
-3   - Consulta Peças por Fabricante
-4   - Retornar""")
+                print("Você Selecionou a Opção de Consultar Peças")
+                print("Escolha a opção desejada:")
+                print("1 - Consultar Todas as Peças")
+                print("2 - Consulta Peças por Código")
+                print("3 - Consulta Peças por Fabricante")
+                print("4 - Retornar\n")
 
                 consulta = int(input())
 
                 if consulta == 1:
 
-                    
                     for cad in cadastro_codigo and cadastro_nome_fab_peca and cadastro_valor:
 
-                
                         print(f"-------------------------\n")
-                        print(f"Código : {cadastro_codigo[cad]:03d}") 
-                        print(f"Nome : {cad}")                  
-                        print(f"Fabricante: {cadastro_nome_fab_peca[cad]}")                    
+                        print(f"Código : {cadastro_codigo[cad]:03d}")
+                        print(f"Nome : {cad}")
+                        print(f"Fabricante: {cadastro_nome_fab_peca[cad]}")
                         print("Valor : %.1f" % (cadastro_valor[cad]))
                         print(f"-------------------------\n")
 
-            
                 elif consulta == 2:
 
-                        entrada_codigo = int(input("Digite o CÓDIGO da Peça:"))
-                    
-                        print(principal.buscar_codigo(entrada_codigo))
+                    entrada_codigo = int(input("Digite o CÓDIGO da Peça:"))
 
-            
+                    print(Principal.buscar_codigo(entrada_codigo))
+
                 elif consulta == 3:
 
-                        entrada_codigo = input("Digite o FABRICANTE da Peça:")
-                    
-                        print(principal.buscar_fabricante(entrada_codigo))
-                
+                    entrada_codigo = input("Digite o FABRICANTE da Peça:")
+
+                    print(Principal.buscar_fabricante(entrada_codigo))
+
                 elif consulta == 4:
 
-                    principal.menu()
+                    Principal.menu()
 
             except(UnboundLocalError, ValueError):
 
@@ -182,22 +167,17 @@ Escolha a opção desejada:
 
     def removerPeca():
 
-    
         while True:
 
-
-                print("""
+            print("""
 Você Selecionou a Opção de Remover Peça
 """)
 
-                entrada_remove = int(input("Digite o codigo da peça a ser removida:"))
-                    
-                
-                print(principal.remover_item(entrada_remove))
+            entrada_remove = int(
+                input("Digite o codigo da peça a ser removida:"))
 
-         
+            print(Principal.remover_item(entrada_remove))
 
 
 id_pessoal()
-principal.menu()
-
+Principal.menu()
